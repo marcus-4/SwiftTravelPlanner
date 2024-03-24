ccaaa34e5aef5eac8a269801caa6f96a5ad35177
marcus-4 <marcus@jkbecker.com>
2024-03-24 14:13:42 -0600

    Initial Commit

---
+++ Becker_Marcus-P2/Becker_Marcus_P2App.swift
@@ -0,0 +1,32 @@
+//
+//  Becker_Marcus_P2App.swift
+//  Becker_Marcus-P2
+//
+//  Created by Marcus Becker on 3/24/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Becker_Marcus_P2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

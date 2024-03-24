ca8078e30b03d1cc90cf310b212b328aa43b636c
marcus-4 <marcus@jkbecker.com>
2024-03-24 14:30:10 -0600

    setup

---
+++ Becker_Marcus-P2/Views/PrototypeView.swift
@@ -0,0 +1,18 @@
+//
+//  PrototypeView.swift
+//  Becker_Marcus-P2
+//
+//  Created by Marcus Becker on 3/24/24.
+//
+
+import SwiftUI
+
+struct PrototypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrototypeView()
+}
